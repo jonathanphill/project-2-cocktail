a2effc4925c8c778ee3894c563deccf8b82b4a03
jonathanphill <jonathanphill@gmail.com>
2023-01-22 13:21:48 -0500

    final revision

--- public/script.js
+++ public/script.js
@@ -11,3 +11,29 @@
 //     }
 // })
 
+let noOfChar = 150;
+let contents = document.querySelectorAll(".content");
+// console.log(contents)
+
+contents.forEach(content => {
+    // if the length is less than noOfChar.then
+    if(content.textContent.length < noOfChar){
+        content.nextElementSibling.style.display = "none";
+    }
+    else{
+        let displayText = content.textContent.slice(0, noOfChar);
+        let moreText = content.textContent.slice(noOfChar);
+        // console.log(displayText);
+        content.innerHTML =`${displayText}<span class="dots">...</span><span class="hide more">${moreText}</span>`;
+    }
+
+});
+
+function readMore(btn){
+    let post = btn.parentElement;
+    // console.log(post);
+    post.querySelector(".dots").classList.toggle("hide");
+    post.querySelector(".more").classList.toggle("hide");
+    btn.textContent == "Read More" ? btn.textContent ="Read Less": btn.textContent ="Read More";
+
+}
